2eec3cb6a169a9150170e1351aeaef47b7b36a17
Sudheer Vootkuri <svootkuri@gmail.com>
2019-03-19 20:06:46 -0500

    attempt1

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "testing with github scm hook as build trigger build3"
+echo "testing with github scm hook as build trigger attempt1"
 echo "this was run automagically"
