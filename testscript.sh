a07cb8414a8c650f965b6368215160c58c3b2a42
Sudheer Vootkuri <svootkuri@gmail.com>
2019-03-20 05:33:05 -0500

    wed-1

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "testing with github scm hook as build trigger attempt3"
-echo "this was run automagically"
+echo "this was run on wed 5:30 am"
