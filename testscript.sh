5778af84b048600815016cf4fad3ffe142afd73a
svootkuri <svootkuri@gmail.com>
2019-03-20 11:51:00 -0500

    added output.txt

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,6 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
+uname -a | awk '{print $2}' >> output.txt
 echo "added user.name and user.email to jenkins config and add webhook in git take 2"
 echo "this was run on wed 5:30 am no luck"
