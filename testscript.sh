9c8235e885e827b30f6150b2ec5e574086b7500f
svootkuri <svootkuri@gmail.com>
2019-03-20 09:39:14 -0500

    add webhook in git

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "added user.name and user.email to jenkins config"
+echo "added user.name and user.email to jenkins config and add webhook in git"
 echo "this was run on wed 5:30 am no luck"
